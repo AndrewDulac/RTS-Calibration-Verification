f6e274bff6af0ef7200952502b7ca6db6846c168
AndrewDulac <andrew.dulac4@gmail.com>
2021-11-30 15:15:23 -0600

    error handling

--- UnitTesting.py
+++ UnitTesting.py
@@ -12,14 +12,27 @@ import numpy as np
 import csv
 import os
 
-wb = load_workbook(os.path.dirname(os.path.realpath(__file__)) + "\\data\\DobleF6150.xlsx")
+fn = fileopenbox()
+#fn = 'C:\\Users\\andre\\Documents\\School\\Fall2021Semester\\ECE 591\\RTS-Calibration-Verification\\data\\11-2-21-CLEAN.xlsx'
+if(fn.endswith('.csv')):
+    wb = Workbook()
+    ws = wb.active
+    with open(fn) as f:
+        reader = csv.reader(f, delimiter=',')
+        for row_index, row in enumerate(reader):
+            for column_index, cell in enumerate(row):
+                column_letter = openpyxl.utils.get_column_letter((column_index + 1))
+                s = cell
+                #Handles heading row or non floats
+                try:
+                    s = float(s)
+                    ws[('%s%s'%(column_letter, (row_index + 1)))].value = s
+                    ws[('%s%s'%(column_letter, (row_index + 1)))].number_format = ""
+                except ValueError:
+                    ws[('%s%s'%(column_letter, (row_index + 1)))].value = cell
+    temppath = os.path.dirname(os.path.realpath(__file__)) + '\\data\\.DataCopy.xlsx'
+    wb.save(temppath)
+else:
+    wb = load_workbook(fn)
 
 ws1 = wb.worksheets[0]
-testdf = pd.read_excel(os.path.dirname(os.path.realpath(__file__)) + "\\DATAFILE.xlsx")
-
-templatedf = pd.read_excel(os.path.dirname(os.path.realpath(__file__)) + "\\data\\DobleF6150.xlsx", )
-
-print(testdf.head(5))
-print(templatedf.head(5))
-
-print("ok")
